6df0086359315626b69ee1d65062cb4b1f96c250
Gobato15 <gobato59@gmail.com>
2026-02-10 22:28:35 -0300

    Criação Objeto Aluno ,AlunoController .php e implementação da listagem em tabela na index.php

--- index.php
+++ index.php
@@ -1,22 +1,46 @@
 <?php
-include_once "configs/database.php";
+include_once "objetos/AlunoController.php";
 
-$banco = new Database();
-$bd = $banco->conectar();
+$controller = new AlunoController();
+$alunos = $controller->index();
+global $alunos;
 
-if ($bd) {
-    $sql = "SELECT * FROM alunos";
-    $resultado = $bd->query($sql);
-    $resultado ->execute();
-    $resultado = $resultado->fetchAll(PDO::FETCH_ASSOC);
+?>
 
-    foreach ($resultado as $aluno) {
-        echo "Nome: ".$aluno['nome'] ."<br>";
-        echo "Email: ".$aluno['email'] ."<br>";
-        echo "Telefone: ".$aluno['telefone'] ."<br>";
-        echo "Login: ". $aluno['login'] ."<br>";
-        echo "Senha: ".$aluno['senha'] ."<br>";
-    }
-}else{
-    echo "Falha ao conectar banco";
-}
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Senac Rio Claro</title>
+</head>
+<body>
+<h1>Senac Rio Claro</h1>
+<h2>Alunos Cadastrados</h2>
+
+<table>
+    <td>RA</td>
+    <td>Nome</td>
+    <td>E-mail</td>
+    <td>Telefone</td>
+    <td>Login</td>
+    </tr>
+    <?php if($alunos) :?>
+    <?php foreach($alunos as $aluno) :?>
+    <tr>
+        <td><?php echo $aluno ->ra; ?></td>
+        <td><?php echo $aluno ->nome; ?></td>
+        <td><?php echo $aluno ->email; ?></td>
+        <td><?php echo $aluno ->telefone; ?></td>
+        <td><?php echo $aluno ->login; ?></td>
+    </tr>
+    <?php endforeach; ?>
+    <?php endif; ?>
+
+</table>
+
+</body>
+
+</html>
+
+
